f032e2de89b4869d437a473a47d909dd0253bf9c
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2024-12-01 15:25:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header/header.js";
+import AboutUs from "./Components/AboutUS/aboutus.js";
+import Manufacture from "./Components/Manufacturer/manufacturer.js";
+import Adthermal from "./Components/Adthermal/adthermal.js";
+import HighPerformanceProduct from "./Components/Header/highperformance.js";
+import Manufactureservices from "./Components/Manufacture-services/manufactureservices.js";
+
+import Contact from "./Components/Contact-us/contact.js";
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/manufacture" element={<Manufacture />}/>
+        <Route path="/adthermal" element={<Adthermal/>}/>
+        <Route path="/HighPerformanceProduct" element={<HighPerformanceProduct/>}/>
+        <Route path="/manufactureservices" element={<Manufactureservices />} />
+
+        <Route path="/contact" element={<Contact />}/>
+
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
